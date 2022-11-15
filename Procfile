bc021073ce23b7b62842e69fc7c39a88ca384db9
vosyliua <vosyliua@coventry.ac.uk>
2022-11-15 04:07:46 +0000

    test1

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: deno run --allow-all --unstable index.js --port=${PORT}
+web: deno run --allow-net=:${PORT} --allow-env=PORT --cached-only index.js
--- index.js
+++ index.js
@@ -2,11 +2,9 @@
 import app from './api/middleware.js'
 import { parse } from 'https://deno.land/std@0.164.0/flags/mod.ts'
 
-const { args } = Deno
-const defaultPort = 8080
-const port = parse(args).port ?? defaultPort;
-console.log(defaultPort + " " + port )
-console.log(typeof(defaultPort) + " " + typeof(port))
+const DEFAULT_PORT = 8080;
+const envPort = Deno.env.get("PORT");
+const port = envPort ? Number(envPort) : DEFAULT_PORT;
 
 app.addEventListener('listen', ({ port }) => console.log(`listening on port: ${port}`))
 
