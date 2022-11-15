74e3f3396427b8e8e139168b313207aeeecb8414
vosyliua <vosyliua@coventry.ac.uk>
2022-11-15 02:59:38 +0000

    heroku

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 
 import app from './api/middleware.js'
-import { Application, send, Status } from "https://deno.land/x/oak@v6.5.1/mod.ts";
-import { Md5 } from 'https://deno.land/std@0.89.0/hash/md5.ts'
-import { parse } from 'https://deno.land/std/flags/mod.ts'
+import * as flags from 'https://deno.land/std/flags/mod.ts'
 
-
+const {args} = Deno
 const defaultPort = 8080
-const { args } = Deno
-const argPort = parse(args).port
+const argPort = flags.parse(args).port;
 const port = argPort ? Number(argPort) : defaultPort
 
 app.addEventListener('listen', ({ port }) => console.log(`listening on port: ${port}`))
