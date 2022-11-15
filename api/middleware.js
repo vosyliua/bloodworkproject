1787cee4210189d81974ce8657978beaac996952
vosyliua <vosyliua@coventry.ac.uk>
2022-11-15 00:49:24 +0000

    heroku

--- api/middleware.js
+++ api/middleware.js
@@ -5,7 +5,7 @@ import { extractCredentials, fileExists, getEtag } from './modules/util.js'
 import { login } from './modules/accounts.js'
 import { contentType } from 'https://deno.land/x/media_types@v3.0.2/mod.ts'
 
-import router from 'routes'
+import router from './api/routes.js'
 
 const app = new Application()
 
