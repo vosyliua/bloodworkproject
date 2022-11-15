33c93a6d123f0277dec256a5966cf8a4ebb7bb6c
vosyliua <vosyliua@coventry.ac.uk>
2022-11-15 02:52:09 +0000

    heroku

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 
 import app from './api/middleware.js'
+import { Application, send, Status } from "https://deno.land/x/oak@v6.5.1/mod.ts";
+import { Md5 } from 'https://deno.land/std@0.89.0/hash/md5.ts'
 import { parse } from 'https://deno.land/std/flags/mod.ts'
+
 
 const defaultPort = 8080
 const { args } = Deno
