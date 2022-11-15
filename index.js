6db489adbdde15e8e131ad8592e77a3ca5d5e310
vosyliua <vosyliua@coventry.ac.uk>
2022-11-15 03:23:13 +0000

    heroku

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ import app from './api/middleware.js'
 import * as flags from 'https://deno.land/std/flags/mod.ts'
 import { parse } from 'https://deno.land/std/flags/mod.ts'
 
-const {args} = Deno
+const { args } = Deno
 const defaultPort = 8080
-const argPort = parse(Deno.args).port
-const port = argPort ? argPort : defaultPort
-console.log(defaultPort + " " + port + " " + argPort)
-console.log(typeof(defaultPort) + " " + typeof(port) + " " + typeof(argPort))
+const port = parse(args).port ?? defaultPort;
+console.log(defaultPort + " " + port )
+console.log(typeof(defaultPort) + " " + typeof(port))
 
 app.addEventListener('listen', ({ port }) => console.log(`listening on port: ${port}`))
 
